d7ce51dde84c1b66a8f368f5476f00d7e30471a2
danis1996 <danisandrios@gmail.com>
2019-08-06 17:14:46 +0300

    On a utiliser 'link_to' au lieu de 'href' dans les liens

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/Home', to: 'static#home'
   get '/Home/create_user', to: 'static#create_user'
   get '/Home/login', to: 'static#login'
-  get '/welcome/:first_name', to: 'static#first_name'
-  get '/gossip/:id', to: 'static#show_gossip'
-  get '/user/:id', to: 'static#show_user'
+  get '/welcome/:first_name', to: 'static#first_name', as:'welcome_first_name'
+  get '/gossip/:id', to: 'static#show_gossip', as: 'gossip_id'
+  get '/user/:id', to: 'static#show_user', as: 'user_id'
 end
